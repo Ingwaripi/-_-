3de720e01f7eefb0c4e57e3655916b151b40bd6e
Ekaterina <ekaterina266226@mail.ru>
2022-08-12 16:10:50 +0300

    Add Task 18

---
+++ Task18/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапозон возможных координат точек этой четверти (х и у).
+
+Console.Write("Введите номер четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string GetReange( int num1)
+{
+    if (num1 == 1) return "x > 0 и y > 0";
+    if (num1 == 2) return "x < 0 и y > 0";
+    if (num1 == 3) return "x < 0 и y < 0";
+    if (num1 == 4) return "x > 0 и y < 0";
+    return "Введите корректный номер четверти";
+}
+string result = GetReange(num);
+Console.WriteLine(result);
