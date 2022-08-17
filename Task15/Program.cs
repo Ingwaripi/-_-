9c10b7f7d50debbc8636bef7d5e8f8d3b31e575e
Ekaterina <ekaterina266226@mail.ru>
2022-08-17 10:14:07 +0300

    add Task 26,15

---
+++ Task15/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу которая на фход принемает число А,
+// на выход выдает сумму чисел
+
+Console.Write("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// int sum = default; //0
+
+// for (int i = 1; i <= num; i++)
+// {
+//     sum = sum + i;
+//     //sum +=i
+// }
+
+//Console.WriteLine($"сумма чисел от 1 до {num} равна {sum}");
+
+int SumNumbers(int numbers)
+{
+    int sum = 0;
+    for (int i = 1; i <= numbers; i++)
+    {
+        sum = sum + i;
+    }   //sum +=i
+        return sum;
+    
+}
+if ( num>0 ){
+    int result = SumNumbers(num);
+    Console.WriteLine($"сумма чисел от 1 до {num} равна {result}");
+}
+else 
+{
+    Console.WriteLine("некоректный ввод, требуется положительное число");
+}
---
+++ Task26/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу которая на вход принемает число,
+// на выходе показывает количестиво цифр
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int DigitNumber(int numbers)
+{
+    int i = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        i++;
+        
+    }
+    return i;
+}
+int result = DigitNumber(num);
+Console.WriteLine(result);
