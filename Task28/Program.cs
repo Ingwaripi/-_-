67950d88d1f3638100712b9e683ebbdfc084a362
Ekaterina <ekaterina266226@mail.ru>
2022-08-21 11:57:14 +0300

    add task 28,30

---
+++ Task28/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу которая на вход принемает число N 
+// и выдает произведение чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= num; i++)
+{
+    result = result * i;
+}
+Console.WriteLine(result);
---
+++ Task30/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу которая выводит массив и 8 элиментов
+// заполненый нулями и единицами в рандомном порядке
+
+int[] gren ()
+{
+    int[] array = new int[8];
+    for (int i = 0; i <= 7; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+int[] array = gren();
+
+for (int i = 0; i <=7; i++)
+{
+    Console.Write($"{array [i]}, ");
+}
