c751298a3f9cabcebdd1d8b1ff00f609ea59cb89
Ekaterina <ekaterina266226@mail.ru>
2022-08-29 13:35:40 +0300

    add TAsk 44

--- Task44/Program.cs
+++ Task44/Program.cs
@@ -9,7 +9,7 @@ void ShowFibonacci (int num)
     int num1 = 0;
     int num2 = 1;
     Console.Write(num1+" "+num2+" ");
-    for (int i = 0; i <= num; i++)
+    for (int i = 2; i < num; i++)
     {
         int temp = num1 + num2;
         num1 = num2;
