194b7cec04ffc7c243b42354927ed2d5acb9f48d
Ekaterina <ekaterina266226@mail.ru>
2022-08-29 12:53:14 +0300

    add TAsk 42

---
+++ Task42/Program.cs
@@ -0,0 +1,14 @@
+﻿// 42. Напишите программуб которая будет преобразовывыть
+//  десятичное число в двоичное
+
+Console.Write("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = default;
+int mult = 1;
+while (num > 0)
+{
+    result += num % 2 * mult;
+    num /= 2;
+    mult *= 10;
+}
+Console.WriteLine(result);
